a67898b66caaeaf49c09ee5c31fb92224730c417
Eloisa <eloisa.anibaletto2@gmail.com>
2022-11-03 21:56:39 -0300

    modificações

---
+++ src/compotens/SobrePag.jsx
@@ -0,0 +1,26 @@
+const SobrePag = () => {
+    //const error = useRouteError();
+    return (
+        <div id="sobre_page" >
+             <img src="https://vgbr.com/wp-content/uploads/2013/05/sims.jpg" alt="logo" />
+            <h1></h1>
+            <p>The Sims (Os Sims em Portugal) é uma série de jogos eletrônicos de simulação</p>
+            <p>de vida real criado pelo designer de jogos Will Wright e produzida pela Maxis.</p>
+            <p>O primeiro jogo da série, The Sims, foi lançado em 4 de fevereiro de 2000. </p>
+            <p>Os jogos da série The Sims são, em grande parte, jogos sandbox, pois não possuem objetivos definidos </p>
+            <p>(exceto alguns pacotes de expansão posteriores e versões de console que introduziram esse estilo de jogo).</p>
+            <p>O jogador cria pessoas virtuais chamadas "Sims" e administra suas necessidades, humores e desejos.</p>
+            <p>Os jogadores podem colocar seus Sims em casas pré-construídas ou construí-las do zero. </p>
+            <p>Também é possível construir lotes comunitários, como praças, bares e restaurantes. </p>
+            <p>Cada pacote de expansão aumenta as possibilidades de seu respectivo jogo base.</p>
+            <p>O jogo atraiu legiões de fãs, devido a sua simplicidade e objetividade. Os jogos foram portados e convertidos </p>
+            <p>para diversas plataformas, incluindo Nintendo, computadores, celulares, Playstation, GameBoy Advance, GameCube, Android, iOS e Xbox.</p>
+            <p>A série The Sims faz parte da Série Sim, iniciada por SimCity, em 1989.</p>
+            <p></p>
+            <h2></h2>
+            <p></p>
+        </div>
+    )
+};
+
+export default SobrePag;
